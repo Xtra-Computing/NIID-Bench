e9d989e990c9603aaf449f7adf4f2f77349ce752
sjtudyq <48618508+sjtudyq@users.noreply.github.com>
2021-02-04 17:08:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 python experiments.py --model=simple-cnn \
     --dataset=cifar10 \
-    --alg=gan \
+    --alg=fedprox \
     --lr=0.01 \
     --batch-size=64 \
     --epochs=10 \
     --n_parties=10 \
     --rho=0.9 \
     --comm_round=50 \
-    --partition=noniid-#label1 \
+    --partition=noniid-labeldir \
     --beta=0.5\
     --device='cuda:0'\
     --datadir='./data/' \
