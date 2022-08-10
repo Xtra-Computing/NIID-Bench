902ef0f1d6bb63e64f9f64c3ee89a07a323390c1
ngdovan <ngdovan@gmail.com>
2022-08-10 21:47:27 +0900

    add utility

--- efNet_utils.py
+++ efNet_utils.py
@@ -1,7 +1,9 @@
 """utils.py - Helper functions for building the model and for loading model parameters.
    These helper functions are built to mirror those in the official TensorFlow implementation.
 """
-
+"""
+Utility for efficientnet
+"""
 # Author: lukemelas (github username)
 # Github repo: https://github.com/lukemelas/EfficientNet-PyTorch
 # With adjustments and added comments by workingcoder (github username).
