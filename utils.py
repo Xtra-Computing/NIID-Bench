278be03354fbbca58792577528093a5d159dd3c4
LWJ <77725034+VinnyLiu0817@users.noreply.github.com>
2023-05-03 23:25:03 +0800

    Update utils.py
    Line 357 should be K instead of num

--- utils.py
+++ utils.py
@@ -354,7 +354,7 @@ def partition_data(dataset, datadir, logdir, partition, n_parties, beta=0.4):
             K = 100
         elif dataset == "tinyimagenet":
             K = 200
-        if num == 10:
+        if K == 10:
             net_dataidx_map ={i:np.ndarray(0,dtype=np.int64) for i in range(n_parties)}
             for i in range(10):
                 idx_k = np.where(y_train==i)[0]
