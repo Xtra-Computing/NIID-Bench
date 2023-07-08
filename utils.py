11b2d3db352bfff2b63c858d7de982962a15474e
sjtudyq <48618508+sjtudyq@users.noreply.github.com>
2023-07-08 16:37:56 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -751,6 +751,9 @@ def get_dataloader(dataset, datadir, train_bs, test_bs, dataidxs=None, noise_lev
             dl_obj = ImageFolder_custom
             transform_train = transforms.Compose([
                 transforms.Resize(32), 
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomRotation(15),
                 transforms.ToTensor(),
                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             ])
